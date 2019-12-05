7e6d7d43c2e02a89fd24faa4d32d6369cfd51633
Gustavo Parreira <gustavo.parreira@mypthub.net>
2019-12-05 12:57:42 +0000

    Changed WebView implementation for react-native-webview.

--- Vimeo.ios.js
+++ Vimeo.ios.js
@@ -4,7 +4,8 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, WebView } from 'react-native';
+import { StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
 
 import package from './package.json';
 
